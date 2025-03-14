f943b4443ee4a76e9a70cf291927b3499afbd9fe
chenxiaoyu <chenxiaoyu@hzpmf.com>
2025-03-14 10:17:42 +0800

    feat: init commit

---
+++ src/main/java/com/xxl/job/plus/executor/config/XxlJobPlusConfig.java
@@ -0,0 +1,13 @@
+package com.xxl.job.plus.executor.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: cxy
+ * @Date: 2025-03-05
+ */
+@Configuration
+@ComponentScan(basePackages = "com.xxl.job.plus.executor")
+public class XxlJobPlusConfig {
+}
